4f76dba159298e49f1f472943c84d67cb983d3a1
Aleksey Zemnitskiy <aleksey.zemnitskiy@portfolioeffect.com>
2015-09-22 18:53:22 -0400

    Updated licensing information

--- README.md
+++ README.md
@@ -3,10 +3,23 @@
 </a>
 
 # PortfolioEffect Quant - Java Library 
-PortfolioEffect Java client library for high frequency portfolio analysis, intraday backtesting and optimization
+
+Java API for PortfolioEffect that allows portfolio analysis, strategy backtesting and portfolio 
+optimization using client and server-side high frequency market data.
 
 ## About PortfolioEffect
 
 PortfolioEffect platform employes high frequency microstructure model pipeline, cloud computing and server-side 
 market data to enable classic portfolio analysis at intraday horizons.
 
+## License
+
+This library is released under the BSD license. See the file LICENSE.
+
+Usage of this library with PortfolioEffect services shall be subject to the [Terms of Service][PortfolioEffect Terms].
+
+## Copyright
+
+Copyright &copy; 2015 PortfolioEffect
+
+[PortfolioEffect Terms]: https://www.portfolioeffect.com/docs/terms
--- src/main/java/com/portfolioeffect/quant/client/ClientConnection.java
+++ src/main/java/com/portfolioeffect/quant/client/ClientConnection.java
@@ -4,18 +4,8 @@
  * %%
  * Copyright (C) 2011 - 2015 Snowfall Systems, Inc.
  * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
  * #L%
  */
 package com.portfolioeffect.quant.client;
@@ -30,7 +20,7 @@ import java.util.Map;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
- 
+
 import org.apache.commons.lang.SerializationUtils;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
@@ -1257,7 +1247,7 @@ public class ClientConnection  {
 
 	private class HeartbeatMonitor implements Runnable {
 
-	
+		@Override
 		public void run() {
 
 			while (!Thread.interrupted()) {
